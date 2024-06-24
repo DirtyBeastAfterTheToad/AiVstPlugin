61d85255de2fb788da9950c5fd97b8f097d1e62a
DBAT <ryan.lemaitre@hotmail.com>
2024-06-24 22:01:53 +0200

    refactor (sinewavevoice): separating h and cpp for sinewavevoice

--- Source/SineWaveVoice.h
+++ Source/SineWaveVoice.h
@@ -4,79 +4,15 @@
 class SineWaveVoice : public juce::SynthesiserVoice
 {
 public:
-	bool canPlaySound(juce::SynthesiserSound* sound) override
-	{
-		return dynamic_cast<SineWaveSound*> (sound) != nullptr;
-	}
+	bool canPlaySound(juce::SynthesiserSound* sound);
 	void startNote(int midiNoteNumber, float velocity,
-		juce::SynthesiserSound*, int /*currentPitchWheelPosition*/) override
-	{
-		currentAngle = 0.0;
-		level = velocity * 0.15;
-		tailOff = 0.0;
+		juce::SynthesiserSound*, int /*currentPitchWheelPosition*/) override;
+	
 
-		auto cyclesPerSecond = juce::MidiMessage::getMidiNoteInHertz(midiNoteNumber);
-		auto cyclesPerSample = cyclesPerSecond / getSampleRate();
+	void renderNextBlock(juce::AudioSampleBuffer& outputBuffer, int startSample, int numSamples) override;
 
-		angleDelta = cyclesPerSample * 2.0 * juce::MathConstants<double>::pi;
-	}
+	void stopNote(float /*velocity*/, bool allowTailOff) override;
 
-	void renderNextBlock(juce::AudioSampleBuffer& outputBuffer, int startSample, int numSamples) override
-	{
-		if (angleDelta != 0.0)
-		{
-			if (tailOff > 0.0)
-			{
-				while (--numSamples >= 0)
-				{
-					auto currentSample = (float)(std::sin(currentAngle) * level * tailOff);
-
-					for (auto i = outputBuffer.getNumChannels(); --i >= 0;)
-						outputBuffer.addSample(i, startSample, currentSample);
-
-					currentAngle += angleDelta;
-					++startSample;
-
-					tailOff *= 0.99;
-
-					if (tailOff <= 0.005)
-					{
-						clearCurrentNote();
-
-						angleDelta = 0.0;
-						break;
-					}
-				}
-			}
-			else
-			{
-				while (--numSamples >= 0)
-				{
-					auto currentSample = (float)(std::sin(currentAngle) * level);
-
-					for (auto i = outputBuffer.getNumChannels(); --i >= 0;)
-						outputBuffer.addSample(i, startSample, currentSample);
-
-					currentAngle += angleDelta;
-					++startSample;
-				}
-			}
-		}
-	}
-
-	void stopNote(float /*velocity*/, bool allowTailOff) override
-	{
-		if (allowTailOff)
-		{
-			if (tailOff == 0.0)
-				tailOff = 1.0;
-		}
-		else
-		{
-			clearCurrentNote();
-			angleDelta = 0.0;
-		}
-	}
 	void pitchWheelMoved(int) override {}
 	void controllerMoved(int, int) override {}
 private:
